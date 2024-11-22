5782e8e7b234860b153f86284279b3577edfef57
Chenyungchin <b08901061@ntu.edu.tw>
2024-11-21 21:17:12 -0500

    finish and verified RSA

--- verilog/RSA_tb.v
+++ verilog/RSA_tb.v
@@ -1,54 +1,72 @@
 //~ `New testbench
 `timescale  1ns / 1ps
+`define CYCLE 10.0
 
 module RSA_tb;
 
-// parameter Parameters
-parameter PERIOD  = 10;
-parameter WIDTH   = 8;
+// ================= clk generation =====================
+reg clk = 1;
+always #(`CYCLE/2) clk = ~clk;
 
-// parameter Inputs
-reg   clk                                  = 0 ;
-reg   rst_n                                = 0 ;
-reg   [WIDTH-1:0]  p                       = 0 ;
-reg   [WIDTH-1:0]  q                       = 0 ;
-reg   [WIDTH-1:0]  m                       = 0 ;
-reg   KeyGenStart                          = 0 ;
-
-// parameter Outputs
-wire  [WIDTH-1:0]  m_decrypted             ;
-wire  finish                               ;
-
-
-initial
-begin
-    forever #(PERIOD/2)  clk=~clk;
+// ================= rst_n =====================
+reg rst_n;
+initial begin
+    rst_n  =  0;
+    @(posedge clk) rst_n = 1;
 end
 
-initial
-begin
-    #(PERIOD*2) rst_n  =  1;
-end
+// =============== instantiate DUT ====================
+// Inputs
+reg   [ 7:0]  p, q;
+reg   [15:0]  m;
+reg           start;
 
+// Outputs
+wire [15:0]  m_decrypted;
+wire         finish;
 
-RSA rsa0 (
+RSA  rsa0 (
     .clk                     ( clk                      ),
     .rst_n                   ( rst_n                    ),
-    .p                       ( p            [WIDTH-1:0] ),
-    .q                       ( q            [WIDTH-1:0] ),
-    .m                       ( m            [WIDTH-1:0] ),
-    .KeyGenStart             ( KeyGenStart              ),
-
-    .m_decrypted             ( m_decrypted  [WIDTH-1:0] ),
+    .KeyGenStart             ( start                    ),
+    .p                       ( p                        ),
+    .q                       ( q                        ),
+    .m                       ( m                        ),
+    .m_decrypted             ( m_decrypted              ),
     .finish                  ( finish                   )
 );
 
-initial
-begin
+// ================== test sequence ====================
+// sample answer: m_decrypted = 89
+initial begin
+    start = 0;
+    p = 0;
+    q = 0;
+    m = 0;
+    @(posedge clk);
+    start = 1;
+    p = 53;
+    q = 59;
+    m = 89;
+    @(posedge clk);
+    start = 0;
+    p = 0;
+    q = 0;
+    m = 0;
+end
+
+// ================== time out ==========================
+initial begin
+    # (500000 * `CYCLE);
+    $display("\n\033[1;31m=============================================");
+	$display("           Simulation Time Out!      ");
+	$display("=============================================\033[0m");
+	$finish;
+end
+
+initial begin
     $dumpfile("RSA_tb.vcd");
     $dumpvars(0, RSA_tb);
-    #(PERIOD*1000);
-    $finish;
 end
 
 endmodule
